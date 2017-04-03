0e731404faccc40c12a0387dd9bed0a2241d275d
Tosone <i@tosiney.com>
2017-04-03 12:11:44 +0800

    update
    Signed-off-by: Tosone <i@tosiney.com>

--- classical-cryptography/caesar-cipher.py
+++ classical-cryptography/caesar-cipher.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
+
+import sys
+
+from PyQt5.QtWidgets import (QApplication, QLineEdit, QMainWindow, QPushButton,
+                             QRadioButton)
 
 
-class Caesar():
-
+class Application(QMainWindow):
     def __init__(self):
-        super(Caesar, self).__init__()
+        super(Application, self).__init__()
+        self.title = "凯撒密码"
+        self.width = 220
+        self.height = 200
+        self.left = 500
+        self.top = 300
+        self.initUI()
 
-    def select(self):
-        print("凯撒密码\n")
-        print("1.带字符\n2.不带字符\n3.退出\n")
-        prefer = input("请输入你的选择：")
-        try:
-            select = int(prefer)
-        except Exception as e:
-            select = 3
-        cipherText = input("输入密文：\n")
-        if select == 1:
-            self.withCharacter(cipherText)
-        elif select == 2:
-            self.withoutCharacter(cipherText)
-        else:
-            exit(0)
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.radio = QRadioButton("带字符", self)
+        self.radio.move(10, 10)
+        self.radio.setChecked(True)
+        self.radioAnother = QRadioButton("不带字符", self)
+        self.radioAnother.move(10, 30)
+
+        self.button = QPushButton('calc', self)
+        self.button.move(140, 10)
+        self.button.resize(70, 20)
+        self.button.clicked.connect(self.click)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.move(10, 100)
+        self.textbox.resize(200, 20)
+        self.textbox.setPlaceholderText("密文")
+
+        self.resultTextbox = QLineEdit(self)
+        self.resultTextbox.move(10, 130)
+        self.resultTextbox.resize(200, 20)
+        self.resultTextbox.setPlaceholderText("结果")
+
+        self.show()
+
+    def click(self):
+        if self.textbox.text() != "":
+            if self.radio.isChecked():
+                self.withCharacter(self.textbox.text())
+            elif self.radioAnother.isChecked():
+                self.withoutCharacter(self.textbox.text())
 
     def withCharacter(self, cipherText):
         for p in range(127):
@@ -35,11 +63,13 @@ class Caesar():
                     feel = 0
                     break
             if feel == 1:
-                print(str1)
+                self.resultTextbox.setText(str1)
 
     def withoutCharacter(self, cipherText):
-        pass
+        self.resultTextbox.setText("nothing")
 
-if __name__ == "__main__":
-    caesar = Caesar()
-    caesar.select()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Application()
+    sys.exit(app.exec_())
--- classical-cryptography/qwe密码.py
+++ classical-cryptography/qwe密码.py
@@ -1,20 +1,57 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 
-# qwe密码
-##################################################################################################
+import sys
+
+from PyQt5.QtWidgets import (QApplication, QLineEdit, QMainWindow, QPushButton,
+                             QRadioButton)
 
 
-def qwe():
-    j = raw_input("input qwe密文:")
-    z = ""
-    for k in j:
-        n = 0
-        for i in "qwertyuiopasdfghjklzxcvbnm":
-            n = n + 1
-            if i == k:
-                z = z + (chr(n + 96))
-    print z
-    raw_input("请输入任意键继续")
-##################################################################################################
-qwe()
+class Application(QMainWindow):
+    def __init__(self):
+        super(Application, self).__init__()
+        self.title = "qwe加密"
+        self.width = 220
+        self.height = 90
+        self.left = 500
+        self.top = 300
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.button = QPushButton('calc', self)
+        self.button.move(140, 60)
+        self.button.resize(70, 20)
+        self.button.clicked.connect(self.click)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.move(10, 10)
+        self.textbox.resize(200, 20)
+        self.textbox.setPlaceholderText("明文")
+
+        self.resultTextbox = QLineEdit(self)
+        self.resultTextbox.move(10, 35)
+        self.resultTextbox.resize(200, 20)
+        self.resultTextbox.setPlaceholderText("结果")
+
+        self.show()
+
+    def click(self):
+        cipherText = self.textbox.text()
+        if cipherText != "":
+            z = ""
+            for k in cipherText:
+                n = 0
+                for i in "qwertyuiopasdfghjklzxcvbnm":
+                    n = n + 1
+                    if i == k:
+                        z = z + (chr(n + 96))
+            self.resultTextbox.setText(z)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Application()
+    sys.exit(app.exec_())
